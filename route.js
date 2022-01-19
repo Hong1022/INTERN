18f64fac5d87e33c12022f4faacf4d3dd4491737
LimLianHong <lianhong2210@gmail.com>
2022-01-19 14:23:01 +0800

    name change

--- route.js
+++ route.js
@@ -2,8 +2,8 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/", function(req,res,){
-    console.log("Hello I'm on the start game here");
+router.get("/", function(req,res){
+    console.log("Hello I'm on the start page here");
     res.render("index");
 });
 
